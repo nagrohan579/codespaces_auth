16e4fc0255cfff82a6c02a31787cb74ade1c60a7
Rohan Nag <99641791+nagrohan579@users.noreply.github.com>
2025-05-21 08:07:56 +0000

    Update server response message and format environment variable import

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Codespaces boiiii</h1>');
+    res.send('<h1>Server is running!</h1>');
 })
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const app = require('./app')
-const {PORT} = process.env
+const {PORT} = process.env 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
